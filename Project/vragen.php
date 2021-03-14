9261091179385fd5101125f6f87a739f6c320e5a
Wouter289 <62356516+Wouter289@users.noreply.github.com>
2021-03-14 17:18:01 +0100

    Update vragen.php

--- Project/vragen.php
+++ Project/vragen.php
@@ -1,1 +1,34 @@
-
+<?php
+	include('sql.php');
+	include('errors.php');
+	
+	//var_dump($_POST['nextQuestion']);
+	
+	if(!isset($_SESSION["count"])){$_SESSION["count"] = 0;}
+	if($_SESSION["count"] == 80){$_SESSION["count"] = 0;}
+	
+	if(isset($_POST['nextQuestion']))
+	{
+		?>
+			<form action="/Project/vragen.php" method="post">
+				<a><?php getQuestion($_SESSION["count"]);?></a><br>
+				<input type = "text" name="question">
+				<input type = "submit" name="nextQuestion">
+			</form>
+		<?php
+		$_SESSION["count"]++;
+		unset($_POST['nextQuestion']);
+	}
+	
+	else
+	{
+		?>
+			<form action="/Project/vragen.php" method="post">
+				<a><?php getQuestion($_SESSION["count"]);?></a><br>
+				<input type = "text" name="question">
+				<input type = "submit" name="nextQuestion">
+			</form>
+		<?php
+	}
+?>
+	
